dcbae6474ffa5f819d284cf7b7f0bdc993f86e91
ASUS <ASUS@DESKTOP-4GEENEV>
2022-06-14 08:56:59 +0700

    trang

---
+++ QLKS_API/DHSLAPI/Models/phong.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLKSAPI.Models
+{
+    public class phong
+    {
+        public string p_ma { get; set; }
+        public string p_malp { get; set; }
+        public string p_tinhtrang { get; set; }
+        public string p_sophong { get; set; }
+        public int p_solandat { get; set; }
+   
+    }
+}
