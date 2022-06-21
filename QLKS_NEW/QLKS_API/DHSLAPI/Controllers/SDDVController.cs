b8779e9c87252e1b8231bf41635f12d607799fa1
hang24abc <hang24mta@gmail.com>
2022-06-21 23:35:46 +0700

    Hang

---
+++ QLKS_NEW/QLKS_API/DHSLAPI/Controllers/SDDVController.cs
@@ -0,0 +1,73 @@
+﻿using DHSLAPI.Models;
+using QLKSAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DHSLAPI.Controllers
+{
+    public class SDDVController : ApiController
+    {
+        // GET: SDDV
+        private readonly Connection con = new Connection();
+
+        [HttpGet]
+        [Route("api/SddvAll")]
+        public List<SDDV> SDDVSelectAll()
+        {
+            return con.SDDVSelectAll();
+        }
+
+        [HttpGet]
+        [Route("api/SddvAllByID")]
+        public List<SDDV> SDDVSelectAllByID(string MaDK)
+        {
+            return con.SDDVSelectAllByID(MaDK);
+        }
+
+        [HttpGet]
+        [Route("api/SddvByID")]
+        public List<SDDV> SDDVSelectByID(string MaDK, string MaDV)
+        {
+            return con.SDDVSelectByID(MaDK, MaDV);
+        }
+
+        [HttpGet]
+        [Route("api/SDDVSelectTop")]
+        public List<SDDV> SDDVSelectTop(string Top, string Where, string Order)
+        {
+            return con.SDDVSelectTop(Top, Where, Order);
+        }
+
+        [HttpGet]
+        [Route("api/SddvTestByID")]
+        public bool SDDVTestByID(string MaDK, string MaDV)
+        {
+            return con.SDDVTestByID(MaDK, MaDV);
+        }
+
+        [HttpPost]
+        [Route("api/SddvInsert")]
+        public bool SDDVInsert(SDDV sddv)
+        {
+            return con.SDDVInsert(sddv);
+        }
+
+        [HttpDelete]
+        [Route("api/SddvDelete")]
+        public bool SDDVDelete(string MaDK, string MaDV)
+        {
+            return con.SDDVDelete(MaDK, MaDV);
+        }
+
+        [HttpPut]
+        [Route("api/SddvUpdate")]
+        public bool SDDVUpdate(SDDV cv)
+        {
+            return con.SDDVUpdate(cv);
+        }
+    }
+}
---
+++ QLKS_NEW/QLKS_API/DHSLAPI/Models/SDDV.cs
@@ -0,0 +1,18 @@
+﻿using QLKSAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DHSLAPI.Models
+{
+    public class SDDV
+    {
+        public string MaDK { get; set; }
+        public DichVu DichVu { get; set; }
+        public NhanVien NhanVien { get; set; }
+        public DateTime NgaySuDung { get; set; }
+
+        public int SoLuong { get; set; }
+    }
+}
